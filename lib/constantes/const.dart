c32cca2b4bd73960940b1aadab65c47838b7b105
Dave-GP <85894890+Dave-GP@users.noreply.github.com>
2022-03-21 08:56:53 -0600

    first commit

---
+++ lib/constantes/const.dart
@@ -0,0 +1,125 @@
+import 'package:spotifyclone/paginas/principal_app.dart';
+
+
+const List lista = [
+  "Wildlife",
+  "Landscapes",
+  "Pets",
+  "Space views",
+  "Flora"
+];
+
+
+
+const List songs = [
+  {
+    "img": "assets/images/tigre.jpg",
+    
+    "title": "Tigre",
+    "description": "Panthera tigris",
+    
+    "songs": [
+      {"title": "El tigre (Panthera tigris) es una de las especies de la subfamilia de los panterinos (familia Felidae) pertenecientes al género Panthera. ",   },
+    
+    ]
+  },
+  {
+    "img": "assets/images/aguila.jpg",
+    "title": "Aguila",
+    "description": "Harpia harpyja",
+   
+    "songs": [
+      {"title": "El águila harpía es el ave nacional de Panamá y la especie símbolo de la diversidad biológica de Ecuador."},
+      
+      
+    ]
+  },
+  {
+    "img": "assets/images/ballena.jpg",
+    "title": "Ballena Azul",
+    "description": "Balaenoptera musculus",
+    
+    "songs": [
+      {"title": " es una especie de cetáceo misticeto de la familia Balaenopteridae. Su tamaño medio es de entre 24 y 27 metros de longitud y pesan entre 100 y 120 toneladas"},
+      
+      
+    ]
+  },
+  {
+    "img": "assets/images/anaconda.jpg",
+    "title": "Anaconda",
+    "description": "Eunectes murinus.",
+   
+    "songs": [
+      {"title": "es una especie de serpiente constrictora de la familia de las boas (Boidae). Es endémica de los ríos del trópico de Sudamérica."},
+    
+      
+    ]
+  },
+  {
+    "img": "assets/images/hipo.jpg",
+    "title": "Hipopotamo",
+    "description": "Hippopotamus amphibius",
+  
+    "songs": [
+      {"title": "Es un gran mamífero artiodáctilo fundamentalmente herbívoro que habita en el África subsahariana. Es, junto al hipopótamo pigmeo (Choeropsis liberiensis), uno de los dos únicos miembros actuales de la familia Hippopotamidae."},
+      
+      
+    ]
+  },
+  {
+    "img": "assets/images/mach.jpg",
+    
+    "title": "Machu pichu",
+    "description": "La ciudadela de Machu Picchu en Cusco",
+    
+    "songs": [
+      {"title": "Descubierta en el año 1902 por Agustín Lizárraga y dada a conocer al mundo en 1911 por Hiram Bingham, la ciudad “perdida” de los Incas fue construida a mediados del siglo XV d. C por órdenes del Inca Pachacútec.",   },
+    
+    ]
+  },
+  {
+    "img": "assets/images/Chichen.jpg",
+    "title": "Chichén Itzá",
+    "description": "Boca del Pozo de los Brujos del Agua",
+   
+    "songs": [
+      {"title": "Inaugurada en el año 525 d.C., este “castillo”, como denominaron los conquistadores españoles a esta maravilla mundial, sirvió como templo para el dios Kukulkán"},
+      
+      
+    ]
+  },
+  {
+    "img": "assets/images/coli.jpg",
+    "title": "El Coliseo de Roma",
+    "description": "Anfiteatro Flavio ​",
+    
+    "songs": [
+      {"title": "ste anfiteatro, el cual es el tesoro que el Imperio Romano dejó como herencia a la Ciudad Eterna, es una de las 7 maravillas del mundo moderno debido a que es el más grande nunca antes construido en el mundo"},
+      
+      
+    ]
+  },
+  {
+    "img": "assets/images/cristo.jpg",
+    "title": "Cristo Redentor",
+    "description": "Cristo del Corcovado.",
+   
+    "songs": [
+      {"title": "Es la gran estatua de Jesús de Nazaret con los brazos abiertos que se ve desde cualquier punto de la ciudad de Río de Janeiro en Brasil. Este monumento Art Decó de 30 metros de altura en lo más alto del cerro Corcovado fue inaugurado en 1931."},
+    
+      
+    ]
+  },
+  {
+    "img": "assets/images/china.jpg",
+    "title": "Gran Muralla China",
+    "description": "La Gran Muralla en Jinshanling",
+  
+    "songs": [
+      {"title": "Sirvió como fortaleza a base de piedra, ladrillo, madera y tierra apisonada para proteger a este país asiático de las posibles invasiones de los mongoles por el norte. Originalmente llegó a medir más de 21 mil kilómetros cuando fue construida entre los siglos V a.c. y XVI d.c. (¡Más de 2000 años!) y millones de obreros perdieron la vida durante su construcción."},
+      
+      
+    ]
+  },
+];
